0c69c8fe5f533a4444a4d278c59a98b343f1ff11
Bas Langenberg <baslangenberg@gmail.com>
2020-12-07 08:21:32 +0100

    Solve day 1 part 1

---
+++ cmd/2020/07/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type bag struct {
+	name   string
+	amount int
+	bags   []bag
+}
+
+func main() {
+	input, err := os.Open("input")
+	defer input.Close()
+
+	if err != nil {
+		fmt.Printf("Unable to open file: %v\n", err)
+	}
+
+	bags := []bag{}
+	scanner := bufio.NewScanner(input)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		nb := bag{
+			name:   strings.Split(line, " ")[0] + " " + strings.Split(line, " ")[1] + " bag",
+			amount: 1,
+		}
+
+		containingBags := strings.Join(strings.Split(line, " ")[4:], " ")
+		containingBags = strings.Replace(containingBags, ", ", ",", -1)
+
+		for _, bg := range strings.Split(containingBags, ",") {
+			if bg[0] == 'n' {
+				continue
+			}
+
+			amount, err := strconv.Atoi(string(bg[0]))
+			if err != nil {
+				fmt.Printf("Unable to parse as int: %v\n", err)
+			}
+			b := bag{
+				name:   fmt.Sprintf("%v bag", strings.Join(strings.Split(bg, " ")[1:3], " ")),
+				amount: amount,
+			}
+
+			nb.bags = append(nb.bags, b)
+		}
+		bags = append(bags, nb)
+	}
+	foundBags := findContains(bags, "shiny gold bag")
+	uniqFoundBags := make(map[string]bool)
+
+	for _, bag := range foundBags {
+		uniqFoundBags[bag] = true
+	}
+
+	fmt.Println(len(uniqFoundBags))
+}
+
+func findContains(bags []bag, bag string) []string {
+	var found []string
+	for _, bg := range bags {
+		for _, b := range bg.bags {
+			if b.name == bag {
+				found = append(found, bg.name)
+				found = append(found, findContains(bags, bg.name)...)
+			}
+		}
+	}
+
+	return found
+}
