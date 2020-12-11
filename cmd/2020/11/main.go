70ed635db310dd86e61083b5a3bcf2be41fd47aa
Bas Langenberg <baslangenberg@gmail.com>
2020-12-11 23:27:32 +0100

    Day 11 part 2 in progress

--- cmd/2020/11/main.go
+++ cmd/2020/11/main.go
@@ -34,7 +34,7 @@ func main() {
 	cache[count] = plan
 
 	for {
-		fmt.Printf("Pass: %v\n", count)
+		// fmt.Printf("Pass: %v\n", count)
 
 		newplan := make([][]rune, len(cache[count]))
 
@@ -45,8 +45,8 @@ func main() {
 
 		newplan = parseMap(newplan)
 
-		printMap(cache[count])
-		printMap(newplan)
+		//printMap(cache[count])
+		//printMap(newplan)
 
 		if reflect.DeepEqual(newplan, cache[count]) {
 			break
@@ -66,7 +66,48 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Seats occupied: %v\n", countOccupied)
+	fmt.Printf("Part A - Seats occupied: %v\n", countOccupied)
+
+	bCache := make(map[int][][]rune)
+	bCount := 0
+
+	bCache[bCount] = plan
+
+	for {
+		// fmt.Printf("Pass: %v\n", count)
+
+		newplan := make([][]rune, len(bCache[bCount]))
+
+		for i := range cache[count] {
+			newplan[i] = make([]rune, len(bCache[bCount][i]))
+			copy(newplan[i], bCache[bCount][i])
+		}
+
+		newplan = parseMapPartB(newplan)
+
+		//printMap(cache[count])
+		//printMap(newplan)
+
+		if reflect.DeepEqual(newplan, bCache[bCount]) {
+			break
+		}
+
+		bCount++
+		bCache[bCount] = newplan
+	}
+
+	countOccupied = 0
+
+	for _, line := range bCache[bCount] {
+		for _, char := range line {
+			if char == '#' {
+				countOccupied++
+			}
+		}
+	}
+	fmt.Printf("Part B - Seats occupied too high: %v\n", 3010)
+	printMap(bCache[bCount])
+	fmt.Printf("Part B - Seats occupied: %v\n", countOccupied)
 }
 
 func printMap(plan [][]rune) {
@@ -76,6 +117,37 @@ func printMap(plan [][]rune) {
 	for _, i := range plan {
 		fmt.Println(string(i))
 	}
+}
+
+func parseMapPartB(plan [][]rune) [][]rune {
+	newplan := make([][]rune, len(plan))
+
+	for i := range plan {
+		newplan[i] = make([]rune, len(plan[i]))
+		copy(newplan[i], plan[i])
+	}
+
+	for vpos, line := range plan {
+		for hpos, char := range line {
+			if char == '.' {
+				continue
+			}
+			occupied, _ := parseNeighboursPartB(hpos, vpos, plan)
+			// fmt.Printf("Cord: %v,%v - occupied: %v, empty: %v\n", hpos, vpos, occupied, empty)
+			if plan[vpos][hpos] == 'L' {
+				if occupied == 0 {
+					newplan[vpos][hpos] = '#'
+				}
+			}
+			if plan[vpos][hpos] == '#' {
+				if occupied > 4 {
+					newplan[vpos][hpos] = 'L'
+				}
+			}
+		}
+	}
+
+	return newplan
 }
 
 func parseMap(plan [][]rune) [][]rune {
@@ -91,8 +163,8 @@ func parseMap(plan [][]rune) [][]rune {
 			if char == '.' {
 				continue
 			}
-			occupied, empty := parseNeighbours(hpos, vpos, plan)
-			fmt.Printf("Cord: %v,%v - occupied: %v, empty: %v\n", hpos, vpos, occupied, empty)
+			occupied, _ := parseNeighbours(hpos, vpos, plan)
+			// fmt.Printf("Cord: %v,%v - occupied: %v, empty: %v\n", hpos, vpos, occupied, empty)
 			if plan[vpos][hpos] == 'L' {
 				if occupied == 0 {
 					newplan[vpos][hpos] = '#'
@@ -107,6 +179,137 @@ func parseMap(plan [][]rune) [][]rune {
 	}
 
 	return newplan
+}
+
+func parseNeighboursPartB(hpos, vpos int, plan [][]rune) (int, int) {
+	empty := 0
+	occupied := 0
+
+	// Links
+	if hpos > 0 {
+		for pos := hpos - 1; pos > -1; pos-- {
+			if plan[vpos][pos] == 'L' {
+				empty++
+				break
+			}
+			if plan[vpos][pos] == '#' {
+				occupied++
+				break
+			}
+		}
+	}
+
+	// Linksboven
+	if vpos > 0 && hpos > 0 {
+		count := 1
+		for {
+			if vpos-count > -1 && hpos-count > -1 {
+				if plan[vpos-count][hpos-count] == 'L' {
+					empty++
+					break
+				}
+				if plan[vpos-count][hpos-count] == '#' {
+					occupied++
+					break
+				}
+			}
+
+			if vpos-count == -1 || hpos-count == -1 {
+				break
+			}
+			count++
+		}
+	}
+
+	// Boven
+	if vpos > 0 {
+		for pos := vpos - 1; pos > -1; pos-- {
+			if plan[pos][hpos] == 'L' {
+				empty++
+				break
+			}
+			if plan[pos][hpos] == '#' {
+				occupied++
+				break
+			}
+		}
+	}
+
+	// Rechtsboven
+	if hpos < len(plan[vpos])-1 && vpos > 0 {
+		count := 1
+		for pos := vpos - 1; pos > -1; pos-- {
+			if vpos-count > -1 && hpos+count < len(plan[vpos]) {
+				if plan[vpos-count][hpos+count] == 'L' {
+					empty++
+					break
+				}
+				if plan[vpos-count][hpos+count] == '#' {
+					occupied++
+					break
+				}
+			}
+			count++
+		}
+	}
+
+	// Rechts
+	for pos := hpos; pos < len(plan[vpos])-1; pos++ {
+		if plan[vpos][pos+1] == 'L' {
+			empty++
+			break
+		}
+		if plan[vpos][pos+1] == '#' {
+			occupied++
+			break
+		}
+	}
+
+	// Rechtsonder
+	if vpos < len(plan)-1 && hpos < len(plan[vpos])-1 {
+		count := 1
+		for pos := vpos; pos < len(plan)-1; pos++ {
+			if vpos+count <= len(plan)-1 && hpos+count < len(plan[vpos]) {
+				if plan[vpos+count][hpos+count] == 'L' {
+					empty++
+					break
+				}
+				if plan[vpos+count][hpos+count] == '#' {
+					occupied++
+					break
+				}
+			}
+		}
+	}
+
+	// Onder
+	for pos := vpos; pos < len(plan)-1; pos++ {
+		if plan[pos+1][hpos] == 'L' {
+			empty++
+			break
+		}
+		if plan[pos+1][hpos] == '#' {
+			occupied++
+			break
+		}
+	}
+
+	// linksonder
+	count := 1
+	for pos := vpos; pos < len(plan); pos++ {
+		if vpos+count < len(plan) && hpos-count > -1 {
+			if plan[vpos+count][hpos-count] == 'L' {
+				empty++
+				break
+			}
+			if plan[vpos+count][hpos-count] == '#' {
+				occupied++
+				break
+			}
+		}
+	}
+
+	return occupied, empty
 }
 
 func parseNeighbours(hpos, vpos int, plan [][]rune) (int, int) {
@@ -184,11 +387,11 @@ func parseNeighbours(hpos, vpos int, plan [][]rune) (int, int) {
 	}
 
 	// linksonder
-	if vpos > 0 && hpos > 0 {
-		if plan[vpos-1][hpos-1] == 'L' {
+	if vpos < len(plan)-1 && hpos > 0 {
+		if plan[vpos+1][hpos-1] == 'L' {
 			empty++
 		}
-		if plan[vpos-1][hpos-1] == '#' {
+		if plan[vpos+1][hpos-1] == '#' {
 			occupied++
 		}
 	}
--- cmd/2020/11/main_test.go
+++ cmd/2020/11/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_parseNeighbours(t *testing.T) {
 	type args struct {
@@ -37,6 +39,144 @@ func Test_parseNeighbours(t *testing.T) {
 			}
 			if got1 != tt.want1 {
 				t.Errorf("parseNeighbours() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_parseNeighboursPartB(t *testing.T) {
+	type args struct {
+		hpos int
+		vpos int
+		plan [][]rune
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  int
+		want1 int
+	}{
+		{
+			name: "lb",
+			args: args{
+				plan: [][]rune{
+					[]rune{'#', '.', '.'},
+					[]rune{'.', '.', '.'},
+					[]rune{'.', '.', '.'},
+				},
+				hpos: 1,
+				vpos: 1,
+			},
+			want:  1,
+			want1: 0,
+		},
+		{
+			name: "b",
+			args: args{
+				plan: [][]rune{
+					[]rune{'.', '#', '.'},
+					[]rune{'.', '.', '.'},
+					[]rune{'.', '.', '.'},
+				},
+				hpos: 1,
+				vpos: 1,
+			},
+			want:  1,
+			want1: 0,
+		},
+		{
+			name: "rb",
+			args: args{
+				plan: [][]rune{
+					[]rune{'.', '.', '#'},
+					[]rune{'.', '.', '.'},
+					[]rune{'.', '.', '.'},
+				},
+				hpos: 1,
+				vpos: 1,
+			},
+			want:  1,
+			want1: 0,
+		},
+		{
+			name: "l",
+			args: args{
+				plan: [][]rune{
+					[]rune{'.', '.', '.'},
+					[]rune{'#', '.', '.'},
+					[]rune{'.', '.', '.'},
+				},
+				hpos: 1,
+				vpos: 1,
+			},
+			want:  1,
+			want1: 0,
+		},
+		{
+			name: "r",
+			args: args{
+				plan: [][]rune{
+					[]rune{'.', '.', '.'},
+					[]rune{'.', '.', '#'},
+					[]rune{'.', '.', '.'},
+				},
+				hpos: 1,
+				vpos: 1,
+			},
+			want:  1,
+			want1: 0,
+		},
+		{
+			name: "lo",
+			args: args{
+				plan: [][]rune{
+					[]rune{'.', '.', '.'},
+					[]rune{'.', '.', '.'},
+					[]rune{'#', '.', '.'},
+				},
+				hpos: 1,
+				vpos: 1,
+			},
+			want:  1,
+			want1: 0,
+		},
+		{
+			name: "o",
+			args: args{
+				plan: [][]rune{
+					[]rune{'.', '.', '.'},
+					[]rune{'.', '.', '.'},
+					[]rune{'.', '#', '.'},
+				},
+				hpos: 1,
+				vpos: 1,
+			},
+			want:  1,
+			want1: 0,
+		},
+		{
+			name: "ro",
+			args: args{
+				plan: [][]rune{
+					[]rune{'.', '.', '.'},
+					[]rune{'.', '.', '.'},
+					[]rune{'.', '.', '#'},
+				},
+				hpos: 1,
+				vpos: 1,
+			},
+			want:  1,
+			want1: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := parseNeighboursPartB(tt.args.hpos, tt.args.vpos, tt.args.plan)
+			if got != tt.want {
+				t.Errorf("parseNeighboursPartB() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("parseNeighboursPartB() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
