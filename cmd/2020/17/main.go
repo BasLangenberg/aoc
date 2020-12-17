da2a13c3d25f3625cf3abf564daf5904245a7a35
Bas Langenberg <baslangenberg@gmail.com>
2020-12-17 19:19:55 +0100

    Added more code, not more solutions

--- cmd/2020/17/main.go
+++ cmd/2020/17/main.go
@@ -1,12 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type conwaycube struct {
 	x, y, z int
 }
 
 type ccubes map[conwaycube]bool
+
+type cubedynamic struct {
+	xmin, xmax, ymin, ymax, zmin, zmax int
+}
 
 func main() {
 	cubes := make(ccubes)
@@ -57,19 +64,23 @@ func main() {
 			}
 
 			cubes[cc] = active
-			cubes[mincc] = active
-			cubes[pluscc] = active
+			cubes[mincc] = false
+			cubes[pluscc] = false
 		}
 	}
 
 	for i := 0; i < 6; i++ {
-		pulse(cubes)
+		bound := getMinMax(cubes)
+		fmt.Println(bound)
+		pulse(cubes, bound)
+		printGrid(cubes, bound)
 	}
 
 	var count int
 
-	for _, val := range cubes {
+	for key, val := range cubes {
 		if val {
+			fmt.Printf("%+v - %v\n", key, val)
 			count++
 		}
 	}
@@ -79,9 +90,84 @@ func main() {
 
 }
 
-// TODO: Loop through coordinates instead of through map!
-func pulse(c ccubes) {
+func printGrid(c ccubes, b cubedynamic) {
+	for z := b.zmin; z <= b.zmax; z++ {
+		fmt.Printf("x: %v", z)
+		for y := b.ymin; y <= b.ymax; y++ {
+
+			for x := b.xmin; x <= b.xmax; x++ {
+				cc := conwaycube{
+					z: z,
+					x: x,
+					y: y,
+				}
+
+				val, ok := c[cc]
+				if !ok {
+					fmt.Printf(".")
+					break
+				}
+
+				if val {
+					fmt.Printf("#")
+					break
+				}
+
+				fmt.Printf(".")
+			}
+
+			fmt.Printf("\n")
+
+		}
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+	}
+}
+
+func getMinMax(c ccubes) cubedynamic {
+	zmax := math.MinInt64
+	ymax := math.MinInt64
+	xmax := math.MinInt64
+
+	zmin := math.MaxInt64
+	ymin := math.MaxInt64
+	xmin := math.MaxInt64
+
+	for key := range c {
+		if zmax < key.z {
+			zmax = key.z
+		}
+		if ymax < key.y {
+			ymax = key.y
+		}
+		if xmax < key.x {
+			xmax = key.x
+		}
+		if zmin > key.z {
+			zmin = key.z
+		}
+		if ymin > key.y {
+			ymin = key.y
+		}
+		if xmin > key.x {
+			xmin = key.x
+		}
+	}
+
+	return cubedynamic{
+		xmin: xmin,
+		xmax: xmax,
+		ymin: ymin,
+		ymax: ymax,
+		zmin: zmin,
+		zmax: zmax,
+	}
+}
+
+func pulse(c ccubes, b cubedynamic) {
 	updates := make(map[conwaycube]bool)
+
 	for key, val := range c {
 		var active int
 		for z := key.z - 1; z <= key.z+1; z++ {
@@ -92,13 +178,14 @@ func pulse(c ccubes) {
 						y: y,
 						z: z,
 					}
+
 					if cc == key {
 						break
 					}
 
 					val, ok := c[cc]
 					if !ok {
-						c[cc] = false
+						updates[cc] = false
 						break
 					}
 
