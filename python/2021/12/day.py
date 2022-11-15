3a16f2ffefe87f3be5ab8785ef2eb1e5cf50c894
Bas Langenberg <baslangenberg@gmail.com>
2022-11-15 22:19:37 +0100

    Updates

--- python/2021/12/day.py
+++ python/2021/12/day.py
@@ -1,4 +1,8 @@
-def graph(inputFile):
+import resource, sys
+resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
+sys.setrecursionlimit(10**6)
+
+def setup_graph(inputFile):
     # Get all nodes
     nodes = set()
     for x in open(inputFile):
@@ -6,7 +10,50 @@ def graph(inputFile):
         for x in line:
             nodes.add(str.strip(x))
 
-    print(nodes)
+    g={}
 
+    for x in nodes:
+        g[x] = list()
 
-graph("input-tst")
+    for x in open(inputFile):
+        line = str.split(str.strip(x), "-")
+        g[line[0]].append(line[1])
+        g[line[1]].append(line[0])
+
+    return g
+
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if start not in graph:
+        return []
+    paths = []
+    for node in graph[start]:
+        if node.isupper() or node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
+def find_all_paths_part2(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if start not in graph:
+        return []
+    paths = []
+    for node in graph[start]:
+        if path.count(node) >= 2 and node.islower:
+            return []
+        if node.isupper() or node != "start":
+            newpaths = find_all_paths_part2(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
+graph = setup_graph("input")
+#print(graph)
+#print(find_all_paths(graph, "start", "end"))
+print(len(find_all_paths(graph, "start", "end")))
+print(len(find_all_paths_part2(graph, "start", "end")))
