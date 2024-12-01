bea9ebf4bb06c6d1d44c41ef4bd0b91e07cdbf1d
Bas Langenberg <baslangenberg@gmail.com>
2024-12-01 13:22:04 +0100

    adds 2024 day 1

---
+++ python/2024/01/1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+left = list()
+right = list()
+
+with open("test-input") as f:
+    for l in f.readlines():
+        l = l.replace("\n", "")
+        x = l.split("   ")
+        left.append(int(x[0]))
+        right.append(int(x[1]))
+
+left.sort()
+right.sort()
+
+sum = 0
+for i in range(len(left)):
+    sum += abs(int(left[i]) - int(right[i]))
+
+print(f"part 1: %s" % sum)
+
+sum = 0
+left = list()
+right = list()
+
+
+with open("input") as f:
+    for l in f.readlines():
+        l = l.replace("\n", "")
+        x = l.split("   ")
+        left.append(int(x[0]))
+        right.append(int(x[1]))
+
+for i in left:
+    count = 0
+    for l in right:
+        if l == i:
+            count += 1
+
+    print(f"{i} * {count} = {i*count}")
+    sum += i * count
+
+
+print(f"part 2: %s" % sum)
+
+
